4f1415a762c8a2c8a04e04d2df335af317f913ae
Michael Davis <mcarsondavis@gmail.com>
2021-08-11 09:42:25 -0500

    muck around with some compilation errors

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::io::{self, Read};
 extern crate tera;
 use tera::Context;
 use tera::Tera;
+// use regex::Regex;
 
 fn main() -> io::Result<()> {
     let args: Vec<_> = std::env::args().collect();
 
     let mut template = String::new();
     io::stdin().read_to_string(&mut template)?;
+    let template: &str = &template[..];
 
     let mut tera = Tera::default();
-    let mut context = Context.new();
+    let mut context = Context::new();
 
     // read matches with regex, load those into context with context.insert/1;
 
@@ -19,7 +21,7 @@ fn main() -> io::Result<()> {
 
     tera.add_raw_template("template.txt", template)?;
 
-    let result = Tera::one_off(template, context, false);
+    let result = tera.render("template.txt", &context);
 
     println!("{}", result);
 
