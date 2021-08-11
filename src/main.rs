cf09b1a7189f0e24afe1f80686cfd3b41b333369
Michael Davis <mcarsondavis@gmail.com>
2021-08-11 08:42:06 -0500

    run rust formatter on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use std::io::{self, Read};
 extern crate tera;
-use tera::Tera;
 use tera::Context;
+use tera::Tera;
 
 fn main() -> io::Result<()> {
-  let args: Vec<_> = std::env::args().collect();
+    let args: Vec<_> = std::env::args().collect();
 
-  let mut template = String::new();
-  io::stdin().read_to_string(&mut template)?;
+    let mut template = String::new();
+    io::stdin().read_to_string(&mut template)?;
 
-  let mut tera = Tera::default();
-  let mut context = Context.new();
+    let mut tera = Tera::default();
+    let mut context = Context.new();
 
-  // read matches with regex, load those into context with context.insert/1;
+    // read matches with regex, load those into context with context.insert/1;
 
-  // example. this filter already exists:
-  // tera.register_filter("upper", string::upper);
+    // example. this filter already exists:
+    // tera.register_filter("upper", string::upper);
 
-  tera.add_raw_template("template.txt", template)?;
+    tera.add_raw_template("template.txt", template)?;
 
-  let result = Tera::one_off(template, context, false);
+    let result = Tera::one_off(template, context, false);
 
-  println!("{}", result);
+    println!("{}", result);
 
-  Ok(())
+    Ok(())
 }
